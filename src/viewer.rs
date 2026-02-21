bf1a82c8cee5f4e5b679748a2e38f6df0ccf3e70
acetim <lilevii1978@gmail.com>
2026-02-21 05:13:38 +0200

    restructured viewer functions to be more modular and work alongside the editor

--- src/viewer.rs
+++ src/viewer.rs
@@ -1,62 +1,70 @@
 
 use crossterm::terminal::enable_raw_mode;
 use crossterm::terminal::disable_raw_mode;
-use std::io::{self, Read, Write, stdout, Stdout};
-use crossterm::event;
+use std::io::{self, Write, stdout, Stdout};
+
 use crossterm::cursor;
-use crossterm::event::{Event, KeyCode};
+use crossterm::event::{ KeyCode, KeyEvent};
 use crossterm::ExecutableCommand;
 
 pub struct Viewer{
-
+    terminal_size:(u16,u16),
+    stdout: Stdout
 }
 impl Viewer{
-    pub fn view(){
-        Self::init();
-
-
-        Self::start_viewer().unwrap();
+    pub fn new() -> Self {
+        Self {terminal_size:crossterm::terminal::size().unwrap_or((20,20)), stdout:stdout()}
     }
-    fn init(){
+    pub fn init(&self){
         enable_raw_mode().unwrap();
+        self.clear_screen().unwrap();
+
     }
-    fn start_viewer()-> io::Result<()>{
-
-        let mut stdout = stdout();
-        let start_col = crossterm::terminal::size().unwrap_or((20,20)).0;
-
-        stdout.execute(cursor::MoveToColumn(start_col))?;
-        stdout.execute(cursor::MoveLeft(1))?;
-
-        Self::handle_until_esc(&mut stdout,start_col)?;
-        disable_raw_mode()?;
-        Ok(())
+    pub fn close_viewer(&self){
+        self.clear_screen_for_viewer_closure().unwrap();
+        disable_raw_mode().unwrap();
     }
-    fn handle_until_esc(stdout:&mut Stdout,start_col:u16)->io::Result<()>{
-        loop {
-            if let Event::Key(key_event) = event::read()? {
-                match key_event.code {
-                    KeyCode::Esc => break,
-                    KeyCode::Char(c) => {
-                     print!("{}", c);
-                     stdout.flush()?;
-                     stdout.execute(cursor::MoveLeft(2))?;
-                 }
-                    KeyCode::Enter=>{
-                     print!("\n\r");
-                     stdout.execute(cursor::MoveToColumn(start_col))?;
-                     stdout.execute(cursor::MoveLeft(1))?;
-                 }
-                    KeyCode::Backspace => {
-                     stdout.execute(cursor::MoveRight(1))?;
-                     print!(" ");
-                     stdout.execute(cursor::MoveLeft(1))?;
-                 }
-                    _ => {}
-                }
-                stdout.flush()?;
+    pub fn read_to_viewer(&mut self,key_event: &KeyEvent)->io::Result<()>{
+        match key_event.code {
+            KeyCode::Char(c) => {
+                print!("{}", c);
+                self.stdout.flush()?;
+                self.stdout.execute(cursor::MoveLeft(2))?;
             }
+            KeyCode::Enter=>{
+                print!("\n\r");
+                self.stdout.execute(cursor::MoveToColumn(self.terminal_size.0))?;
+                print!(" ");
+                self.stdout.execute(cursor::MoveLeft(1))?;
+            }
+            KeyCode::Backspace => {
+                self.stdout.execute(cursor::MoveRight(1))?;
+                print!(" ");
+                self.stdout.execute(cursor::MoveLeft(1))?;
+            }
+            _ => {}
         }
+        self.stdout.flush()?;
         Ok(())
  }
+    fn clear_screen(&self)->io::Result<()>{
+        let cols = self.terminal_size.0;
+         for _rows in 0..self.terminal_size.1{
+             stdout().execute(cursor::MoveToColumn(cols))?;
+             print!("~");
+             print!("\n")
+         }
+        stdout().execute(cursor::MoveToRow(0))?;
+        print!(" ");
+        stdout().execute(cursor::MoveLeft(1))?;
+        Ok(())
+    }
+    fn clear_screen_for_viewer_closure(&self)-> io::Result<()>{
+        stdout().execute(cursor::MoveToColumn(0))?;
+        for _rows in 0..self.terminal_size.1*2{
+            print!("\n")
+        }
+        Ok(())
+    }
+
 }
