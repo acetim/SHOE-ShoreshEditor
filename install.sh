ee3b8f4bda85edea804bd3a306f54eb884138360
acetim <Lilevii1978@gmail.com>
2026-03-01 15:56:20 +0200

    install script hebrew fix

--- install.sh
+++ install.sh
@@ -1,23 +1,30 @@
-
+#!/bin/bash
 
 if ! command -v cargo &> /dev/null; then
-  echo "מוריד cargo";
+    echo "...ןיקתהל הסנמ ,אצמנ אל Cargo"
+    
     if command -v apt &> /dev/null; then
-      sudo apt update && sudo apt install -y cargo;
+        sudo apt update && sudo apt install -y cargo
     elif command -v pacman &> /dev/null; then
-      sudo pacman -S --noconfirm cargo
-    fi
+        sudo pacman -S --noconfirm cargo
     else
-      echo "לא יכול לזהות distro"
-      echo "אנא הורד cargo ידנית"
-      exit 1
-
+        echo ".(ךומתנ אל distro) תולבח המהנמ תה אזל ניתן אל :האיגש"
+        echo ".בוש ץרה ןכמ רחאלו /rs.putsur//:sptth-מ תינדי cargo/rust התקה אנא"
+        exit 1
+    fi
+else
+    echo ".הקתהה בשלב לע גלדמ ,ןקותמ רבכ Cargo"
 fi
 
-echo "מקמפל אפליקציה..."
+echo "...הייצקילפא לפמקמ"
 cargo build -q
 
-cp ./target/debug/SHOE-ShoreshEditor /usr/bin/shoreshed
-
-echo "וזהו!"
-echo "השתמש ב shoreshed ואז את שם הקובץ שתרצה ליצור בכדי להתחיל לערוך!"
+if [ $? -eq 0 ]; then
+    echo "...bin-ל קיתעמ .החיולצה הייקילפומקה"
+    sudo cp ./target/debug/SHOE-ShoreshEditor /usr/bin/shoreshed
+    echo "!והזו"
+    echo "!ךורעל ליחתהל ידכב רוציל הצתר ש ץבוקה מש תא זאו shoreshed-ב שמתשה"
+else
+    echo ".הלשכנ הייצילפומקה :האיגש"
+    exit 1
+fi
